0f990517c0e042f9bac0d0442e578b03a0129e53
guilherme <guilherme.cristaldo15@gmail.com>
2025-02-05 16:28:39 -0400

    feat: user controller create

--- src/modules/users/controller/UserControllers.ts
+++ src/modules/users/controller/UserControllers.ts
@@ -1,11 +1,21 @@
-import { Request, Response } from "express";
-import ListUserService from "../service/ListUserService";
+import { Request, Response } from 'express';
+import ListUserService from '../service/ListUserService';
+import CreateUserService from '../service/CreateUserService';
 
 export default class UserControllers {
-  async index(req: Request, res: Response): Promise<void> {
+  async index(_req: Request, res: Response): Promise<void> {
     const listUserService = new ListUserService();
     const users = await listUserService.execute();
 
     res.json(users);
+  }
+
+  async create(req: Request, res: Response): Promise<void> {
+    const { name, email, password } = req.body;
+
+    const createUserService = new CreateUserService();
+    const user = await createUserService.execute({ name, email, password });
+
+    res.json(user);
   }
 }
