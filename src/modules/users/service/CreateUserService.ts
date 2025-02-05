d87ce5994588b57b0ed1a353f4ca7a2416375fb9
guilherme <guilherme.cristaldo15@gmail.com>
2025-02-05 16:13:06 -0400

    feat: create user service and hash pass with bcrypt

---
+++ src/modules/users/service/CreateUserService.ts
@@ -0,0 +1,30 @@
+import AppError from '@shared/errors/AppError';
+import { userRepositories } from '../database/entities/UsersRepositories';
+import { hash } from 'bcrypt';
+import { User } from '../database/entities/User';
+
+interface ICreateUserService {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export default class CreateUserService {
+  async execute({ name, email, password }: ICreateUserService): Promise<User> {
+    const emailExists = await userRepositories.findByEmail(email);
+
+    if (emailExists) throw new AppError('This email already exists', 409);
+
+    const hashedPassword = await hash(password, 10);
+
+    const user = userRepositories.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await userRepositories.save(user);
+
+    return user;
+  }
+}
