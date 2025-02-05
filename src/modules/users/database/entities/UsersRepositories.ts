694b42ee5e80b11502f692f2d9b952d3ca4a0ca3
guilherme <guilherme.cristaldo15@gmail.com>
2025-02-05 15:22:28 -0400

    feat: user repositories

---
+++ src/modules/users/database/entities/UsersRepositories.ts
@@ -0,0 +1,12 @@
+import AppDataSource from '@shared/typeorm/data-source';
+import { User } from './User';
+
+export const userRepositories = AppDataSource.getRepository(User).extend({
+  async findByName(name: string): Promise<User | null> {
+    return this.findOneBy({ name });
+  },
+
+  async findById(id: number): Promise<User | null> {
+    return this.findOneBy({ id });
+  },
+});
