0bb41c736ef910fc7ded132f0e765d73c9494872
guilherme <guilherme.cristaldo15@gmail.com>
2025-02-07 09:15:05 -0400

    feat: static route to files

--- src/shared/http/router/index.ts
+++ src/shared/http/router/index.ts
@@ -1,10 +1,10 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
 import { Request, Response } from 'express';
-
 import productsRouter from '@modules/products/routes/ProductRoutes';
 import sessionRouter from '@modules/users/routes/SessionRoutes';
 import userRouter from '@modules/users/routes/UserRoutes';
 import avatarRouter from '@modules/users/routes/AvatarRoutes';
+import uploadConfig from '@config/uploads';
 
 const routes = Router();
 
@@ -16,5 +16,6 @@ routes.use('/products', productsRouter);
 routes.use('/users', userRouter);
 routes.use('/login', sessionRouter);
 routes.use('/avatar', avatarRouter);
+routes.use('/files', express.static(uploadConfig.directory))
 
 export default routes;
