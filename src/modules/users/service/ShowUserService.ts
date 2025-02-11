ee2697868fdb08b794e0d0fdab25ba184f878650
CristaldoGui <guilherme.cristaldo15@gmail.com>
2025-02-11 14:37:10 -0400

    show user profile service

---
+++ src/modules/users/service/ShowUserService.ts
@@ -0,0 +1,17 @@
+import AppError from "@shared/errors/AppError";
+import { userRepositories } from "../database/repositories/UsersRepositories";
+import { User } from "../database/entities/User";
+
+interface IShowUserProfile {
+  user_id: number;
+}
+
+export default class ShowUserProfile{
+  async execute({user_id}: IShowUserProfile): Promise<User> {
+    const user = await userRepositories.findById(user_id);
+
+    if(!user) throw new AppError("User not found", 404);
+
+    return user;
+  }
+}
